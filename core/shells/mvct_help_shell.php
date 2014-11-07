0ba7186e43ffff91b4fc7b88eec75221173e010a
rivayama <tomozo16@gmail.com>
2014-11-07 14:49:34 +0900

    modify help

--- core/shells/mvct_help_shell.php
+++ core/shells/mvct_help_shell.php
@@ -6,18 +6,20 @@ class MvctHelpShell {
 		echo "Usage: mvct COMMAND [command-specific-options]\n";
 		echo "\n";
 		echo "COMMAND:\n";
+		echo "\n";
 		echo "help               # Show this message\n";
 		echo "generate [options] # Generate scaffold, element, template\n";
 		echo "\n";
 		echo "Generate options:\n";
-		echo "theme [theme-name]           # Theme scaffold\n";
-		echo "template [template-name]     # Template\n";
-		echo "element [element-name]       # MVC and test files\n";
-		echo "controller [controller-name] # Controller\n";
-		echo "model [model-name]           # Model\n";
-		echo "view [view-name]             # View\n";
-		echo "test [test-name]             # Test\n";
-		echo "fixture [fixture-name]       # Fixture yaml\n";
+		echo "\n";
+		echo "theme [theme-name]                        # Theme scaffold\n";
+		echo "template [theme-name] [template-name]     # Template\n";
+		echo "element [theme-name] [element-name]       # M,V,C and test files\n";
+		echo "controller [theme-name] [controller-name] # Controller\n";
+		echo "model [theme-name] [model-name]           # Model\n";
+		echo "view [theme-name] [view-name]             # View\n";
+		echo "test [theme-name] [test-name]             # Test\n";
+		echo "fixture [theme-name] [fixture-name]       # Fixture yaml\n";
 	}
 
 }
