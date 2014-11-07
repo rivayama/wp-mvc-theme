50a3b33f8a96eec2dfeef34ab38bc97b1a92c9f5
rivayama <tomozo16@gmail.com>
2014-11-07 14:44:01 +0900

    Modify shell to be specified theme name

--- core/shells/mvct_generate_shell.php
+++ core/shells/mvct_generate_shell.php
@@ -6,13 +6,18 @@ class MvctGenerateShell {
 		$this->templater = new MvctTemplater;
 	}
 
-	public function theme($name) {
-		$vars = array(
-			'name_camel' => MvctInflector::camelize($name),
-			'name_snake' => MvctInflector::snakize($name),
-		);
-
-		$theme_path = get_theme_root().'/'.$vars['name_snake'];
+	public function theme($args) {
+		list($theme_name) = $args;
+		if (! $theme_name) {
+			echo "Theme name should be specified.\n";
+			exit();
+		}
+		if ($this->theme_exists($theme_name)) {
+			echo "Theme `$theme_name` already exists.\n";
+			die();
+		}
+		$vars = $this->get_vars($theme_name);
+		$theme_path = $this->get_theme_path($theme_name);
 		$directories = array(
 			'elements',
 			'elements/controllers',
@@ -22,94 +27,118 @@ class MvctGenerateShell {
 			'tests',
 			'tests/fixtures',
 		);
-
-		if (file_exists($theme_path)) {
-			echo "Theme `$name` already exists.";
-			return;
-		}
 		mkdir($theme_path);
 		foreach ($directories as $dir) {
 			mkdir($theme_path.'/'.$dir);
 		}
-
 		$this->templater->create('theme/style', $theme_path.'/style.css', $vars);
 		$this->templater->create('theme/header', $theme_path.'/header.php', $vars);
 		$this->templater->create('theme/sidebar', $theme_path.'/sidebar.php', $vars);
 		$this->templater->create('theme/footer', $theme_path.'/footer.php', $vars);
 		$this->templater->create('theme/phpunit', $theme_path.'/phpunit.xml', $vars);
 		$this->templater->create('template', $theme_path.'/index.php', $vars);
-		echo "Theme `$name` created.";
+		echo "Theme `$theme_name` created.\n";
 	}
 
-	public function template($name) {
-		$vars = array(
+	public function template($args) {
+		if (! $this->validate_args($args, __METHOD__)) die();
+		list($theme_name, $target) = $args;
+		$vars = $this->get_vars($target);
+		$file = $this->get_theme_path($theme_name).'/'.$vars['name_snake'].'.php';
+		$this->templater->create('template', $file, $vars);
+		echo $this->templater->get_message();
+	}
+
+	public function element($args) {
+		$this->controller($args);
+		$this->model($args);
+		$this->view($args);
+		$this->test($args);
+	}
+
+	public function controller($args) {
+		if (! $this->validate_args($args, __METHOD__)) die();
+		list($theme_name, $target) = $args;
+		$vars = $this->get_vars($target);
+		$file = $this->get_theme_path($theme_name).'/elements/controllers/'.$vars['name_snake'].'_controller.php';
+		$this->templater->create('controller', $file, $vars);
+		echo $this->templater->get_message();
+	}
+
+	public function model($args) {
+		if (! $this->validate_args($args, __METHOD__)) die();
+		list($theme_name, $target) = $args;
+		$vars = $this->get_vars($target);
+		$file = $this->get_theme_path($theme_name).'/elements/models/'.$vars['name_snake'].'.php';
+		$this->templater->create('model', $file, $vars);
+		echo $this->templater->get_message();
+	}
+
+	public function view($args) {
+		if (! $this->validate_args($args, __METHOD__)) die();
+		list($theme_name, $target) = $args;
+		$vars = $this->get_vars($target);
+		$file = $this->get_theme_path($theme_name).'/elements/views/'.$vars['name_snake'].'.php';
+		$this->templater->create('view', $file, $vars);
+		echo $this->templater->get_message();
+	}
+
+	public function test($args) {
+		if (! $this->validate_args($args, __METHOD__)) die();
+		list($theme_name, $target) = $args;
+		$vars = $this->get_vars($target);
+		$file = $this->get_theme_path($theme_name).'/tests/'.$vars['name_camel'].'Test.php';
+		$this->templater->create('test', $file, $vars);
+		echo $this->templater->get_message();
+	}
+
+	public function fixture($args) {
+		if (! $this->validate_args($args, __METHOD__)) die();
+		list($theme_name, $target) = $args;
+		$vars = $this->get_vars($target);
+		$file = $this->get_theme_path($theme_name).'/tests/fixtures/'.$vars['name_snake'].'.yml';
+		$this->templater->create('fixture', $file, $vars);
+		echo $this->templater->get_message();
+	}
+
+	// {{{ Privates
+
+	private function get_vars($name) {
+		return array(
 			'name_camel' => MvctInflector::camelize($name),
 			'name_snake' => MvctInflector::snakize($name),
 		);
-		$path = MVCT_CURRENT_THEME_PATH;
-		$file = $vars['name_snake'].'.php';
-		$this->templater->create('template', $path.'/'.$file, $vars);
-		echo $this->templater->get_message();
 	}
 
-	public function element($name) {
-		$this->controller($name);
-		$this->model($name);
-		$this->view($name);
-		$this->test($name);
+	private function get_theme_path($theme_name) {
+		return get_theme_root().'/'.MvctInflector::snakize($theme_name);
 	}
 
-	public function controller($name) {
-		$vars = array(
-			'name_camel' => MvctInflector::camelize($name),
-			'name_snake' => MvctInflector::snakize($name),
-		);
-		$path = MVCT_CURRENT_THEME_PATH.'/elements/controllers';
-		$file = $vars['name_snake'].'_controller.php';
-		$this->templater->create('controller', $path.'/'.$file, $vars);
-		echo $this->templater->get_message();
+	private function theme_exists($theme_name) {
+		$theme_path = $this->get_theme_path($theme_name);
+		if (file_exists($theme_path)) {
+			return true;
+		}
+		return false;
 	}
 
-	public function model($name) {
-		$vars = array(
-			'name_camel' => MvctInflector::camelize($name),
-		);
-		$path = MVCT_CURRENT_THEME_PATH.'/elements/models';
-		$file = MvctInflector::snakize($name).'.php';
-		$this->templater->create('model', $path.'/'.$file, $vars);
-		echo $this->templater->get_message();
+	private function validate_args($args, $method) {
+		list($theme_name, $target) = $args;
+		if (! $theme_name) {
+			echo "Theme name should be specified.\n";
+			return false;
+		}
+		if (! $target) {
+			echo "$method name should be specified.\n";
+			return false;
+		}
+		if (! $this->theme_exists($theme_name)) {
+			echo "Theme `$theme_name` does not exists.\n";
+			return false;
+		}
+		return true;
 	}
 
-	public function view($name) {
-		$vars = array(
-			'name_camel' => MvctInflector::camelize($name),
-			'name_snake' => MvctInflector::snakize($name),
-		);
-		$path = MVCT_CURRENT_THEME_PATH.'/elements/views';
-		$file = $vars['name_snake'].'.php';
-		$this->templater->create('view', $path.'/'.$file, $vars);
-		echo $this->templater->get_message();
-	}
-
-	public function test($name) {
-		$vars = array(
-			'name_camel' => MvctInflector::camelize($name),
-		);
-		$path = MVCT_CURRENT_THEME_PATH.'/tests';
-		$file = $vars['name_camel'].'Test.php';
-		$this->templater->create('test', $path.'/'.$file, $vars);
-		echo $this->templater->get_message();
-	}
-
-	public function fixture($name) {
-		$vars = array(
-			'name_camel' => MvctInflector::camelize($name),
-			'name_snake' => MvctInflector::snakize($name),
-		);
-		$path = MVCT_CURRENT_THEME_PATH.'/tests/fixtures';
-		$file = $vars['name_snake'].'.yml';
-		$this->templater->create('fixture', $path.'/'.$file, $vars);
-		echo $this->templater->get_message();
-	}
+	// }}}
 
 }
--- core/shells/mvct_shell_dispatcher.php
+++ core/shells/mvct_shell_dispatcher.php
@@ -12,7 +12,6 @@ class MvctShellDispatcher {
 	private function dispatch($args) {
 		$shell_name = ($args[1]) ? $args[1] : 'help';
 		$method = ($args[2]) ? $args[2] : 'main';
-		$name = ($args[3]) ? $args[3] : '';
 
 		$title = 'mvct_'.$shell_name.'_shell';
 		$class = MvctInflector::camelize($title);
@@ -31,12 +30,8 @@ class MvctShellDispatcher {
 			$this->dispatch(array('', 'help'));
 		}
 
-		if ($shell_name != 'help' && ! $name) {
-			$this->illegal_option($args[3]);
-			$this->dispatch(array('', 'help'));
-		}
-
-		$obj->$method($name);
+		$options = array_slice($args, 3);
+		$obj->$method($options);
 		die();
 	}
 
